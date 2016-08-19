7826b976225522faeab902ce9703ac1dbb9c763b
fomjar <fomjar@gmail.com>
2016-08-19 13:31:15 +0800

    makeintr参数整理，所有参数可设置，优化封面图显示，增加阴影

--- src/ski/ski-tools/src/main/java/com/ski/tools/MakeIntrExecutor.java
+++ src/ski/ski-tools/src/main/java/com/ski/tools/MakeIntrExecutor.java
@@ -12,8 +12,10 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -22,21 +24,16 @@ import com.ski.common.bean.BeanGame;
 
 public class MakeIntrExecutor implements ToolExecutor {
 	
-	private static final int 	COVER_WIDTH 	= 160;
-	private static final int 	COVER_HEIGHT 	= 160;
-	
-	private static final Font	FONT			= new Font("黑体", Font.PLAIN, 20);
-	private static final Color  BACKGROUND		= new Color(2, 0, 20);
-	private static final Color  FOREGROUND		= Color.white;
-//	private static final Color  BACKGROUND		= Color.white;
-//	private static final Color  FOREGROUND		= Color.black;
-	private static final int 	TOP				= 150;
-	private static final int 	MARGIN			= 30;
-	
-	private static final int 	HEAD_HEIGHT 	= TOP + MARGIN + COVER_HEIGHT;
-	private static final int 	POSTER_HEIGHT	= 400;
-	
-	private static int width = 0;
+	private static int 		g_width 	= 750;
+	private static String 	g_dir		= ".";
+	private static int 		g_top		= 150;
+	private static int 		g_margin	= 30;
+	private static int 		g_cover 	= 180;
+	private static int 		g_poster	= 400;
+	private static Color  	g_bg		= new Color(2, 0, 20);
+	private static Color  	g_fg		= Color.white;
+	private static Font		g_font		= new Font("黑体", Font.PLAIN, 20);
+	private static String	g_format	= "jpg";
 	
 	public MakeIntrExecutor() {
 		CommonService.setWsiHost("ski.craftvoid.com");
@@ -44,14 +41,26 @@ public class MakeIntrExecutor implements ToolExecutor {
 
 	@Override
 	public void execute(String[] args) {
-		width 	= Integer.parseInt(args[0]);
-        String  dir  	= args.length > 1 ? args[1] : ".";
+		for (int i = 0; i < args.length; i++) {
+			String name;
+			     if (args[i].startsWith(name = "width="))	g_width 	= Integer.parseInt(args[i].substring(name.length()));
+			else if (args[i].startsWith(name = "top=")) 	g_top 		= Integer.parseInt(args[i].substring(name.length()));
+			else if (args[i].startsWith(name = "margin=")) 	g_margin 	= Integer.parseInt(args[i].substring(name.length()));
+			else if (args[i].startsWith(name = "cover=")) 	g_cover 	= Integer.parseInt(args[i].substring(name.length()));
+			else if (args[i].startsWith(name = "poster=")) 	g_poster 	= Integer.parseInt(args[i].substring(name.length()));
+			else if (args[i].startsWith(name = "bg=")) 		g_bg 		= new Color(Integer.parseInt(args[i].substring(name.length())));
+			else if (args[i].startsWith(name = "fg=")) 		g_fg 		= new Color(Integer.parseInt(args[i].substring(name.length())));
+			else if (args[i].startsWith(name = "font=")) 	g_font 		= g_font.deriveFont(Float.parseFloat(args[i].substring(name.length())));
+			else if (args[i].startsWith(name = "dir=")) 	g_dir 		= args[i].substring(name.length());
+			else if (args[i].startsWith(name = "format=")) 	g_format 	= args[i].substring(name.length());
+			else System.out.println("unknown argument: " + args[i]);
+		}
 		
 		System.out.print(String.format("%-40s", "fetching game data..."));
 		CommonService.updateGame();
 		System.out.println(" done!");
 		
-		CommonService.getGameAll().values().parallelStream().forEach(game->makeIntr(game, width, dir));
+		CommonService.getGameAll().values().parallelStream().forEach(game->makeIntr(game, g_width, g_dir));
 	}
 	
 	private static void makeIntr(BeanGame game, int width, String dir) {
@@ -59,29 +68,32 @@ public class MakeIntrExecutor implements ToolExecutor {
 		
 		String[] posters = game.c_url_poster.split(" ");
 		if (1 == posters.length && 0 == posters[0].length()) posters = new String[] {};
-		String[] introduction = convertArticle(game.c_introduction, width - MARGIN * 2);
-		int height = HEAD_HEIGHT
-				+ MARGIN	// separator
-				+ (0 == posters.length ? 0 : posters.length * (MARGIN + POSTER_HEIGHT))
-				+ (MARGIN + introduction.length * FONT.getSize() * 2)
-				+ MARGIN;
+		String[] introduction = convertArticle(game.c_introduction, width - g_margin * 2);
+		int height = g_top																// top
+				+ g_margin + g_cover													// cover
+				+ g_margin																// separator
+				+ (0 == posters.length ? 0 : posters.length * (g_margin + g_poster))	// poster
+				+ (g_margin + introduction.length * g_font.getSize() * 2)				// introduction
+				+ g_margin;																// bottom
 		
 		// 初始化缓存区
-		BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		Graphics g = buffer.getGraphics();
-		g.setColor(BACKGROUND);
+		g.setColor(g_bg);
 		g.fillRect(0, 0, buffer.getWidth(), buffer.getHeight());
 		
 		// 初始设定
-		g.setFont(FONT);
+		g.setFont(g_font);
 		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g.setColor(FOREGROUND);
+		g.setColor(g_fg);
 		
 		int[] current = new int[] {0};
+		current[0] = g_top;
 		// 画封面
 		drawCover(current, g, game.c_url_cover);
 		// 画字段
 		drawField(current, g, game);
+		current[0] = g_top + g_margin + g_cover;
 		// 画分割线
 		drawSeparator(current, g);
 		// 画第一张海报
@@ -94,7 +106,7 @@ public class MakeIntrExecutor implements ToolExecutor {
 		}
 		
 		try {
-			ImageIO.write(buffer, "png", new File(dir + "/output/" + game.c_name_zh_cn + ".png"));
+			ImageIO.write(buffer, g_format, new File(String.format("%s/output/%s.%s", dir, game.c_name_zh_cn, g_format)));
 			System.out.println(" done!");
 		} catch (IOException e) {
 			System.out.println(" fail!");
@@ -104,97 +116,99 @@ public class MakeIntrExecutor implements ToolExecutor {
 	
 	private static void drawCover(int[] current, Graphics g, String url) {
 		Image cover = downloadImage(url);
-		int bg_width = width;
-		int bg_height = width / cover.getWidth(null) * cover.getHeight(null);
+		// background
+		drawBackground(g, cover);
+		// watermark
+		drawWatermark(g, "VC电玩");
+		
+		// shadow
+		int offset = 6;
+		g.setColor(new Color(0, 0, 0, 80));
+		g.fillRect(g_margin + offset, current[0] + g_margin + offset, g_cover, g_cover);
+		// cover
+		g.setColor(g_fg);
+		g.drawImage(cover, g_margin, current[0] + g_margin, g_cover, g_cover, null);
+		// mask
+		((Graphics2D) g).setPaint(new GradientPaint(
+				new Point(g_margin, current[0] + g_margin),
+				new Color(255, 255, 255, 150),
+				new Point(g_margin + g_cover / 2, current[0] + g_margin + g_cover),
+				new Color(255, 255, 255, 0)));
+		g.fillRect(g_margin, current[0] + g_margin, g_cover, g_cover);
+		g.setColor(new Color(255, 255, 255, 100));
+		g.drawLine(g_margin, current[0] + g_margin, g_margin + g_cover - 1, current[0] + g_margin);
+		g.drawLine(g_margin, current[0] + g_margin, g_margin, current[0] + g_margin + g_cover - 1);
+		g.setColor(new Color(0, 0, 0, 100));
+		g.drawLine(g_margin, current[0] + g_margin + g_cover - 1, g_margin + g_cover - 1, current[0] + g_margin + g_cover - 1);
+		g.drawLine(g_margin + g_cover - 1, current[0] + g_margin, g_margin + g_cover - 1, current[0] + g_margin + g_cover - 1);
+		
+		g.setColor(g_fg);
+	}
+	
+	private static void drawBackground(Graphics g, Image cover) {
+		int bg_width = g_width;
+		int bg_height = g_width / cover.getWidth(null) * cover.getHeight(null);
 		// background
 		g.drawImage(cover, 0, 0, bg_width, bg_height, null);
 		((Graphics2D) g).setPaint(new GradientPaint(
 				new Point(0, 0),
-				new Color(BACKGROUND.getRed(), BACKGROUND.getGreen(), BACKGROUND.getBlue(), 100),
-				new Point(0, HEAD_HEIGHT + MARGIN * 4),
-				new Color(BACKGROUND.getRed(), BACKGROUND.getGreen(), BACKGROUND.getBlue(), 255)));
+				new Color(g_bg.getRed(), g_bg.getGreen(), g_bg.getBlue(), 100),
+				new Point(0, g_top + g_margin + g_cover + g_margin * 4),
+				new Color(g_bg.getRed(), g_bg.getGreen(), g_bg.getBlue(), 255)));
 		g.fillRect(0, 0, bg_width, bg_height);
-		// 画水印
-		drawWatermark(g, "VC电玩");
-		// cover
-		g.setColor(FOREGROUND);
-		g.drawImage(cover, MARGIN, current[0] + TOP + MARGIN, COVER_WIDTH, COVER_HEIGHT, null);
-		// mask
-		((Graphics2D) g).setPaint(new GradientPaint(
-				new Point(MARGIN, current[0] + TOP + MARGIN),
-				new Color(255, 255, 255, 150),
-				new Point(MARGIN + COVER_WIDTH / 2, current[0] + TOP + MARGIN + COVER_HEIGHT),
-				new Color(255, 255, 255, 0)));
-		g.fillRect(MARGIN, current[0] + TOP + MARGIN, COVER_WIDTH, COVER_HEIGHT);
-		g.setColor(new Color(255, 255, 255, 100));
-		g.drawLine(MARGIN, current[0] + TOP + MARGIN, MARGIN + COVER_WIDTH - 1, current[0] + TOP + MARGIN);
-		g.drawLine(MARGIN, current[0] + TOP + MARGIN, MARGIN, current[0] + TOP + MARGIN + COVER_HEIGHT - 1);
-		g.setColor(new Color(0, 0, 0, 100));
-		g.drawLine(MARGIN, current[0] + TOP + MARGIN + COVER_HEIGHT - 1, MARGIN + COVER_WIDTH - 1, current[0] + TOP + MARGIN + COVER_HEIGHT - 1);
-		g.drawLine(MARGIN + COVER_WIDTH - 1, current[0] + TOP + MARGIN, MARGIN + COVER_WIDTH - 1, current[0] + TOP + MARGIN + COVER_HEIGHT - 1);
-		
-		g.setColor(FOREGROUND);
 	}
 	
 	private static void drawField(int[] current, Graphics g, BeanGame game) {
-		int base_x 		= COVER_WIDTH + MARGIN * 2 + 10;
-		int count  		= 5;
+		Map<String, String> fields = new LinkedHashMap<String, String>();
+		fields.put("中文名称：", game.c_name_zh_cn);
+		fields.put("英文名称：", game.c_name_en);
+		fields.put("支持语言：", game.c_language);
+		fields.put("游戏大小：", game.c_size);
+		fields.put("发售时间：", game.t_sale);
+		fields.put("发行厂商：", game.c_vendor);
+		
+		int base_x 		= g_cover + g_margin * 2;
+		int count  		= fields.size();
 		int label_len 	= g.getFont().getSize() * 6;
-		int field_len   = width - base_x - label_len - MARGIN;
-		int interval_y 	= COVER_HEIGHT / count;
-		int offset_y    = (interval_y - g.getFont().getSize()) / 2;
+		int field_len   = g_width - base_x - label_len - g_margin;
+		int interval_y 	= g_cover / count;
+		int offset_y    = (interval_y - g.getFont().getSize()) / 3;
 		
-		current[0] += TOP + MARGIN + g.getFont().getSize() + offset_y;
-		String label = "中文名称：";
-		drawShadowString(g, label, 				base_x, 			current[0], field_len);
-		drawShadowString(g, game.c_name_zh_cn, 	base_x + label_len, current[0], field_len);
-		current[0] += interval_y;
-		label = "英文名称：";
-		drawShadowString(g, label, 				base_x, 			current[0], field_len);
-		drawShadowString(g, game.c_name_en, 		base_x + label_len, current[0], field_len);
-		current[0] += interval_y;
-		label = "游戏大小：";
-		drawShadowString(g, label, 				base_x, 			current[0], field_len);
-		drawShadowString(g, game.c_size,	 		base_x + label_len, current[0], field_len);
-		current[0] += interval_y;
-		label = "发售时间：";
-		drawShadowString(g, label, 				base_x, 			current[0], field_len);
-		drawShadowString(g, game.t_sale, 			base_x + label_len, current[0], field_len);
-		current[0] += interval_y;
-		label = "发行厂商：";
-		drawShadowString(g, label, 				base_x, 			current[0], field_len);
-		drawShadowString(g, game.c_vendor, 		base_x + label_len, current[0], field_len);
-		
-		current[0] = TOP + MARGIN + COVER_HEIGHT;
+		current[0] += g_margin + g.getFont().getSize() + offset_y;
+		fields.forEach((label, field)->{
+			drawShadowString(g, label,	base_x, 			current[0], label_len);
+			drawShadowString(g, field, 	base_x + label_len, current[0], field_len);
+			current[0] += interval_y;
+		});
 	}
 	
 	private static void drawSeparator(int[] current, Graphics g) {
-		current[0] += MARGIN;
-		g.setColor(BACKGROUND.darker());
-		g.drawLine(MARGIN + 1, current[0] + 1, width - MARGIN + 1, current[0] + 1);
-		g.setColor(FOREGROUND);
-		g.drawLine(MARGIN, current[0], width - MARGIN, current[0]);
+		current[0] += g_margin;
+		g.setColor(g_bg.darker());
+		g.drawLine(g_margin + 1, current[0] + 1, g_width - g_margin + 1, current[0] + 1);
+		g.setColor(g_fg);
+		g.drawLine(g_margin, current[0], g_width - g_margin, current[0]);
 	}
 	
 	private static void drawShadowString(Graphics g, String s, int x, int y, int width) {
-		g.setColor(BACKGROUND.darker());
+		g.setColor(g_bg.darker());
 		drawString(g, s, x + 2, y + 2, width);
-		g.setColor(FOREGROUND);
+		g.setColor(g_fg);
 		drawString(g, s, x, y, width);
 	}
 	
 	private static void drawPoster(int[] current, Graphics g, String poster) {
 		Image img = downloadImage(poster);
-		current[0] += MARGIN;
-		g.drawImage(img, MARGIN, current[0], width - MARGIN * 2, POSTER_HEIGHT, null);
-		current[0] += POSTER_HEIGHT;
+		current[0] += g_margin;
+		g.drawImage(img, g_margin, current[0], g_width - g_margin * 2, g_poster, null);
+		current[0] += g_poster;
 	}
 	
 	private static void drawIntroduction(int[] current, Graphics g, String[] introduction) {
-		current[0] += MARGIN;
+		current[0] += g_margin;
 		current[0] += g.getFont().getSize();
 		for (String s : introduction) {
-			drawShadowString(g, s, MARGIN, current[0], width - MARGIN * 2);
+			drawShadowString(g, s, g_margin, current[0], g_width - g_margin * 2);
 			current[0] += g.getFont().getSize() * 2;
 		}
 		current[0] -= g.getFont().getSize();
@@ -203,22 +217,21 @@ public class MakeIntrExecutor implements ToolExecutor {
 	private static void drawWatermark(Graphics g, String watermark) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 1000; i++) sb.append(watermark + "  ");
-		String[] wms = convertArticle(sb.toString(), width);
+		String[] wms = convertArticle(sb.toString(), g_width);
 		g.setFont(g.getFont().deriveFont(45.0f));
 		g.setColor(new Color(255, 255, 255, 8));
-//		int current = TOP + MARGIN + COVER_HEIGHT;
 		int current = 0;
 		for (String wm : wms) {
 			g.drawString(wm, 0, current);
 			current += g.getFont().getSize() * 5 / 4;
 			if (current > 5000) break;
 		}
-		g.setFont(FONT);
+		g.setFont(g_font);
 	}
 	
 	private static String[] convertArticle(String article, int width) {
 		Graphics g = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB).getGraphics();
-		g.setFont(FONT);
+		g.setFont(g_font);
 		
 		int from = 0;
 		String indent = "        ";
