b46c7c5c3666a2a4893c7dc9651d6246fbf0d1e3
fomjar <fomjar@gmail.com>
2016-09-11 17:54:42 +0800

    简介收起函数

--- src/ski/ski-wca/conf/form/lib/ski.js
+++ src/ski/ski-wca/conf/form/lib/ski.js
@@ -6,7 +6,7 @@ var ski = {
             return $.cookie(key);
         case 2:
             var origin = $.cookie(key);
-            $.cookie(key, val, { path: "/"});
+            $.cookie(key, val, {path: "/"});
             return origin;
         }
     },
@@ -27,6 +27,25 @@ var ski = {
             cover.append("<div style='position: absolute; z-index: 9; top: 0px; left: 0px; right: 0px; bottom: 0px; border-top: 1px solid lightgray; border-left: 1px solid lightgray; border-bottom: 1px solid black; border-right: 1px solid black'></div>");
             */
             return cover;
+        },
+        
+        createTextWithHeight : function(text, height) {
+            var id=new Date().getTime();
+            var id_txt = 'txt_' + id;
+            var id_btn = 'btn_' + id;
+            var div = $("<div></div>");
+            div.append("<div id='" + id_txt + "' style='width:100%;overflow:hidden;text-overflow:ellipsis;'>" + text + "</div>");
+
+            var fn = "if ($('#"+id_txt+"').height() > "+height+") {"
+                       + "$('#"+id_txt+"').attr('dh', $('#"+id_txt+"').height());"
+                       + "$('#"+id_btn+"').text('展开');"
+                       + "$('#"+id_txt+"').animate({height : '"+height+"px'});"
+                   + "} else {"
+                       + "$('#"+id_btn+"').text('收起');"
+                       + "$('#"+id_txt+"').animate({height : $('#"+id_txt+"').attr('dh')+'px'});"
+                   + "}";
+            div.append("<div style='text-align:right'><a id='"+id_btn+"' href=\"javascript: " + fn + "\">收起</a></div>");
+            return div;
         }
     }
 };
