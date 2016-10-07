5dd5d4aa6e870c7ac7672b17dd0fe04c4d859c9b
fomjar <fomjar@gmail.com>
2016-10-07 09:08:56 +0800

    性能优化

--- src/fomjar/fomjar-server/bin/startup.bat
+++ src/fomjar/fomjar-server/bin/startup.bat
@@ -1,7 +1,7 @@
 @echo off
 
-::set opt=-J-server -J-Xdebug -J-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8844
-set opt=-J-server
+::set opt=-J-server -J-Xmx128M -J-Xdebug -J-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8844
+set opt=-J-server -J-Xmx128M
 
 cd ..
 java -cp lib\fomjar-server-0.0.1.jar fomjar.server.FjServerLauncher %opt% %1 %2 %3 %4 %5 %6 %7 %8 %9
