42884dc47016e8e776b94ff6907739859a3c7296
fomjar <fomjar@gmail.com>
2017-03-03 21:40:44 +0800

    向量内积算法

---
+++ src/ski-frs/ski-frs-cdb/conf/script/sp/function.sql
@@ -0,0 +1,29 @@
+
+-- 向量内积
+delimiter //
+drop function if exists transvection //
+create function transvection (
+    v1  text,   -- 向量1
+    v2  text,   -- 向量2
+    d   integer -- 向量维数
+)
+returns double
+begin
+	
+	declare    t       double  default 0;  -- transvection
+    declare    i       tinyint default 1;
+	declare    v1d     double  default 0;
+	declare    v2d     double  default 0;
+	
+	while i <= d do
+       set v1d = substring_index(substring_index(v1, ' ', i), ' ', -1);
+       set v2d = substring_index(substring_index(v2, ' ', i), ' ', -1);
+       
+       set t = t + v1d * v2d;
+       set i = i + 1;
+	end while;
+	
+	return t;
+	
+end //
+delimiter ;
