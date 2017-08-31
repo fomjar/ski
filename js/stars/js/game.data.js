9e8b3e76ed1e9ea1bac7125f649ad06ba3faaf80
fomjar <fomjar@gmail.com>
2017-09-01 01:35:59 +0800

    spit data and view

--- js/stars/js/game.data.js
+++ js/stars/js/game.data.js
@@ -4,6 +4,31 @@ define(['tween'], function (tween) {
     
     var data = {};
     
+    data.tween  = function (ta, pr, to, fn, tm) {
+        if (!tm) tm = 160;
+        if (!fn) fn = tween.Circ.easeOut;
+
+        var from    = ta[pr];
+        var time    = 0;
+        var begin   = new Date().getTime();
+
+        if (!ta.tweener) ta.tweener = {};
+        if (ta.tweener[pr]) {
+            document.app.ticker.remove(ta.tweener[pr]);
+            delete ta.tweener[pr];
+        }
+        ta.tweener[pr] = function (delta) {
+            time = new Date().getTime() - begin;
+            ta[pr] = fn(time, from, to - from, tm);
+            if (time >= tm) {
+                ta[pr] = to;
+                document.app.ticker.remove(ta.tweener[pr]);
+                delete ta.tweener[pr];
+            }
+        };
+        document.app.ticker.add(ta.tweener[pr]);
+    };
+
     
     data.Meta = function (type) {
         this.type   = type;
@@ -16,48 +41,33 @@ define(['tween'], function (tween) {
     };
     
     
-    data.Data = function () {
-        this.x  = 0;
-        this.y  = 0;
-        this.scale  = 1;
+    data.Data = function (view) {
+        this.view   = view;
+        this.position   = {x : 0, y : 0};
+        this.__defineGetter__('x', function (x) {return this.position.x; });
+        this.__defineSetter__('x', function (x) {
+            this.position.x = x;
+            if (this.view) this.view.position.x = x;
+        });
+        this.__defineGetter__('y', function (y) {return this.position.y; });
+        this.__defineSetter__('y', function (y) {
+            this.position.y = y;
+            if (this.view) this.view.position.y = y;
+        });
         this.meta   = new data.Meta();
-        this.tick   = function () {};
-        this.tween  = function (dp, to, fn, tm) {
-            if (!tm) tm = 160;
-            if (!fn) fn = tween.Circ.easeOut;
-
-            var self    = this;
-            var from    = self[dp];
-            var time    = 0;
-            var begin   = new Date().getTime();
-
-            if (self.tweener) {
-                document.app.ticker.remove(self.tweener);
-                delete self.tweener;
-            }
-            self.tweener = function (delta) {
-                time = new Date().getTime() - begin;
-                self[dp] = fn(time, from, to - from, tm);
-                if (time >= tm) {
-                    self[dp] = to;
-                    document.app.ticker.remove(self.tweener);
-                    delete self.tweener;
-                }
-            };
-            document.app.ticker.add(self.tweener);
-        };
+        this.tick   = function () {};        
     };
     
     
-    data.DPane = function () {
-        this.__proto__ = new data.Data();
+    data.DPane = function (view) {
+        this.__proto__ = new data.Data(view);
         
         this.width  = 0;
         this.height = 0;
     };
     
-    data.DPaneAsset = function () {
-        this.__proto__ = new data.DPane();
+    data.DPaneAsset = function (view) {
+        this.__proto__ = new data.DPane(view);
         
         this.x      = 150;
         this.y      = 20;
@@ -67,22 +77,20 @@ define(['tween'], function (tween) {
     
     
     
-    data.DButton = function (text) {
-        this.__proto__ = new data.Data();
+    data.DButton = function (view) {
+        this.__proto__ = new data.Data(view);
         
         this.width  = 0;
         this.height = 0;
-        this.text   = text || '';
     }
     
     
     
     
-    data.DStar = function (meta) {
-        this.__proto__  = new data.Data();
+    data.DStar = function (view, meta) {
+        this.__proto__  = new data.Data(view);
         
-        this.radius = Math.random() * 10 + 15;
-        this.meta   = new data['MStar_' + meta]();
+        this.meta   = new data['MStar_' + meta](view);
     };
         
     data.MStar_home = function () {
--- js/stars/js/game.view.js
+++ js/stars/js/game.view.js
@@ -7,33 +7,37 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
     view.View = function () {
         this.__proto__  = new PIXI.Graphics();
         
-        this.data   = new data.Data();
+        this.data   = new data.Data(this);
         this.interactive    = true;
-//        this.buttonMode     = true;
         this.state  = 'default';
-        this.draw = function () {};
+        this.draw   = function () {};
         
         this.auto_scale = function (s) {
-            if (!s) s = 1.05;
+            if (!s) s = 1.1;
             
-            this.on('pointerover',      function () {
-                this.data.tween('scale', s);
+            this.on('pointerover', function () {
+                data.tween(this.scale, 'x', s);
+                data.tween(this.scale, 'y', s);
                 this.state = 'over';
             });
-            this.on('pointerout',       function () {
-                this.data.tween('scale', 1);
+            this.on('pointerout', function () {
+                data.tween(this.scale, 'x', 1);
+                data.tween(this.scale, 'y', 1);
                 this.state = 'default';
             });
-            this.on('pointerdown',      function () {
-                this.data.tween('scale', 1);
+            this.on('pointerdown', function () {
+                data.tween(this.scale, 'x', 1);
+                data.tween(this.scale, 'y', 1);
                 this.state = 'down';
             });
-            this.on('pointerup',        function () {
-                this.data.tween('scale', s);
+            this.on('pointerup', function () {
+                data.tween(this.scale, 'x', s);
+                data.tween(this.scale, 'y', s);
                 this.state = 'over';
             });
             this.on('pointerupoutside', function () {
-                this.data.tween('scale', 1);
+                data.tween(this.scale, 'x', 1);
+                data.tween(this.scale, 'y', 1);
                 this.state = 'default'
             });
         };
@@ -43,7 +47,7 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
     view.VPane = function () {
         this.__proto__ = new view.View();
         
-        this.data   = new data.DPane();
+        this.data   = new data.DPane(this);
         this.draw   = function () {};
     };
     
@@ -51,7 +55,7 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
     view.VPaneAsset = function () {
         this.__proto__ = new view.VPane();
         
-        this.data   = new data.DPaneAsset();
+        this.data   = new data.DPaneAsset(this);
         this.draw   = function () {
             var bg  = 0x9999ff;
             var bd  = 0x999999;
@@ -59,29 +63,36 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
 
             this.beginFill(bg, alpha);
             this.lineStyle(2, bd, alpha);
-            this.drawRect(this.data.x - this.data.width / 2 * this.data.scale,
-                          this.data.y - this.data.height / 2 * this.data.scale,
-                          this.data.width * this.data.scale,
-                          this.data.height * this.data.scale);
+            this.drawRect(- this.data.width / 2,
+                          - this.data.height / 2,
+                          this.data.width,
+                          this.data.height);
             this.endFill();
         };
     }
     
     
-    view.VButton = function () {
+    view.VButton = function (text, action) {
         this.__proto__ = new view.View();
         
-        this.data   = new data.DButton();
+        this.data   = new data.DButton(this);
+        this.text   = new PIXI.Text(text);
+        this.text.pivot.x = this.text.width / 2;
+        this.text.pivot.y = this.text.height / 2;
+        this.addChild(this.text);
+        this.buttonMode = true;
+        
         this.click  = function (action) {
             this.on('pointerup', action);
         };
+        this.click(action);
         
         this.auto_scale();
     };
     
     
     view.VButtonPrimary = function (text, action) {
-        this.__proto__ = new view.VButton();
+        this.__proto__ = new view.VButton(text, action);
         
         this.data.width     = 60;
         this.data.height    = 24;
@@ -100,16 +111,13 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
             }
 
             this.beginFill(bg, this.data.alpha);
-            this.lineStyle(1, bd, this.data.alpha);
-            this.drawRoundedRect(this.data.x - this.data.width / 2 * this.data.scale,
-                          this.data.y - this.data.height / 2 * this.data.scale,
-                          this.data.width * this.data.scale,
-                          this.data.height * this.data.scale, 6);
+            this.lineStyle(2, bd, this.data.alpha);
+            this.drawRoundedRect(- this.data.width / 2,
+                                 - this.data.height / 2,
+                                 this.data.width,
+                                 this.data.height, 6);
             this.endFill();
         }
-        
-        if (text)   this.data.text = text;
-        if (action) this.click(action);
     }
     
 
@@ -117,7 +125,8 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
     view.VStar = function (meta) {
         this.__proto__  = new view.View();
         
-        this.data   = new data.DStar(meta);
+        this.data   = new data.DStar(this, meta);
+        this.radius = Math.random() * 10 + 15;
         this.draw   = function () {view.draw.star[meta](this, this.data);};
         
         this.auto_scale();
@@ -126,13 +135,13 @@ define(['game.data', 'tween', 'pixi'], function (data, tween, PIXI) {
     
     view.draw = {
         star    : {
-            home    : function (g, d) {
+            home    : function (g) {
                 var bg  = 0xffff99;
                 var bd  = 0x999999;
                 
                 g.beginFill(bg, 1);
                 g.lineStyle(2, bd, 1);
-                g.drawCircle(d.x, d.y, d.radius * d.scale);
+                g.drawCircle(0, 0, g.radius);
                 g.endFill();
             },
         },
